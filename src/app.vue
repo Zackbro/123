5a1ed918fa929facae679ead23e8b9c69ad46e65
xiaobai <948206388@qq.com>
2017-03-08 21:07:26 +0800

    add index

--- src/app.vue
+++ src/app.vue
@@ -7,25 +7,49 @@
 
     <!-- Main Views -->
     <f7-views>
-      <f7-view id="main-view">
+      <f7-view id="main-view" navbar-through :dynamic-navbar="true" main>
+        <!-- iOS Theme Navbar -->
+        <f7-navbar v-if="$theme.ios">
+          <f7-nav-left>
+            <f7-link icon="icon-bars" open-panel="left"></f7-link>
+          </f7-nav-left>
+          <f7-nav-center sliding></f7-nav-center>
+          <f7-nav-right>
+            <f7-link icon="icon-bars" open-panel="right"></f7-link>
+          </f7-nav-right>
+        </f7-navbar>
         <!-- Pages -->
         <f7-pages>
           <f7-page>
             <!-- Page Content -->
-            <div class="wrapper">
-              <div class="top">
-                <div class="title">支出</div>
-                <div class="cost">0.00</div>
-              </div>
+            <f7-searchbar
+            cancel-link="Cancel"
+            placeholder="Search in items"
+            :clear-button="true"
+            ></f7-searchbar>
+
+            <div class="media">
+                <media></media>
+                <media></media>
             </div>
+
+
+
           </f7-page>
         </f7-pages>
       </f7-view>
     </f7-views>
 
+
+
   </div>
 </template>
 
 <script>
-export default {}
+import media from './components/Mediaitem'
+  export default {
+    components: {
+      media
+    }
+  }
 </script>
---
+++ src/components/Mediaitem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="index tv">
+    <h2><a class="title" href="">在电视上</a></h2>
+    <div class="column">
+      <div class="info first">
+        <a href="/tv/1402"><img src="https://image.tmdb.org/t/p/w320_and_h180_bestv2/zYFQM9G5j9cRsMNMuZAX64nmUMf.jpg" alt="行尸走肉" sizes="375px"></a>
+        <span>
+          <div class="content">
+            <a class="text" href="/tv/1402">
+              <h3>行尸走肉</h3>
+            </a>
+            <a class="text">
+              <p>New episode airs in 4 days</p>
+            </a>
+          </div>
+        </span>
+      </div>
+      <div class="info second">
+        <a href="/tv/1402"><img class="backdrop fade lazyautosizes lazyloaded" data-sizes="auto" src="https://image.tmdb.org/t/p/w320_and_h180_bestv2/zYFQM9G5j9cRsMNMuZAX64nmUMf.jpg" alt="行尸走肉" sizes="375px"></a>
+        <span>
+          <div class="content">
+            <a class="text" href="/tv/1402">
+              <h3>行尸走肉</h3>
+            </a>
+            <a class="text" alt="Season 7, Episode 13" title="Season 7, Episode 13" href="/tv/1402/season/7/episode/13">
+              <p>New episode airs in 4 days</p>
+            </a>
+          </div>
+        </span>
+      </div>
+      <div class="info third">
+        <a href="/tv/1402"><img class="backdrop fade lazyautosizes lazyloaded" data-sizes="auto" src="https://image.tmdb.org/t/p/w320_and_h180_bestv2/zYFQM9G5j9cRsMNMuZAX64nmUMf.jpg" alt="行尸走肉" sizes="375px"></a>
+        <span>
+          <div class="content">
+            <a class="text" href="/tv/1402">
+              <h3>行尸走肉</h3>
+            </a>
+            <a class="text" alt="Season 7, Episode 13" title="Season 7, Episode 13" href="/tv/1402/season/7/episode/13">
+              <p>New episode airs in 4 days</p>
+            </a>
+          </div>
+        </span>
+      </div>
+    </div>
+  </div>
+  </template>
+
+  <script>
+    export default {}
+  </script>
