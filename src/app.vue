22dcbfd5d8d7d2c7f1671515cf1a00040df3e647
xiaobai <948206388@qq.com>
2017-03-09 18:53:09 +0800

    img nav change

--- src/app.vue
+++ src/app.vue
@@ -11,11 +11,11 @@
         <!-- iOS Theme Navbar -->
         <f7-navbar v-if="$theme.ios">
           <f7-nav-left>
-            <f7-link icon="icon-bars" open-panel="left"></f7-link>
+            <f7-link open-panel="left" class="icon-menu"></f7-link>
           </f7-nav-left>
           <f7-nav-center sliding></f7-nav-center>
           <f7-nav-right>
-            <f7-link icon="icon-bars" open-panel="right"></f7-link>
+            <f7-link open-panel="right" class="icon-user"></f7-link>
           </f7-nav-right>
         </f7-navbar>
         <!-- Pages -->
@@ -23,31 +23,61 @@
           <f7-page>
             <!-- Page Content -->
             <f7-searchbar
-            cancel-link="Cancel"
-            placeholder="Search in items"
+            placeholder="搜索电影、剧集、人物"
             :clear-button="true"
             ></f7-searchbar>
 
             <div class="media">
-                <media></media>
-                <media></media>
+              <div class="index tv">
+                <h2><a class="title" href="">在电视上</a></h2>
+                <media v-for="item in defaultTvData" :item="item"></media>
+              </div>
+              <div class="index moive">
+                <h2><a class="title" href="">在影院里</a></h2>
+                <media v-for="item in defaultMoiveData" :item="item"></media>
+              </div>
             </div>
-
-
-
           </f7-page>
         </f7-pages>
       </f7-view>
     </f7-views>
 
-
-
   </div>
 </template>
 
 <script>
-import media from './components/Mediaitem'
+  import indexData from './utils/constant'
+  import media from './components/Mediaitem'
   export default {
+    data() {
+      return {
+        tvData: indexData.tvData,
+        moiveData: indexData.moiveData,
+        defaultTvData: indexData.defaultTvData,
+        defaultMoiveData: indexData.defaultMoiveData,
+      }
+    },
+    mounted () {
+      // this.$http.get('https://api.themoviedb.org/3/tv/1402?api_key=215d774e3ae6ecca32d95f20baa55cb0').then(function(res) { 
+      //     console.log(res.data);
+      // })
+      // 
+      this.preLoadImg(this.tvData, this.defaultTvData);
+      this.preLoadImg(this.moiveData, this.defaultMoiveData);
+    },
+    methods: {
+      // default img => loaded img
+      preLoadImg (mediaData, defaultData) {
+        let that = this
+        for (let i = 0; i < mediaData.length; i++) {
+          let newImg = new Image()
+          newImg.src = mediaData[i].img;
+          newImg.onload = function() {
+            defaultData[i].img = mediaData[i].img;
+          }
+        }
+      }
+    },
     components: {
       media
     }
---
+++ src/utils/constant.js
@@ -0,0 +1,64 @@
+import defaultImg from '../images/bg.jpg'
+
+const indexData = {
+  tvData: [
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/zYFQM9G5j9cRsMNMuZAX64nmUMf.jpg'
+  }, 
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/mmxxEpTqVdwBlu5Pii7tbedBkPC.jpg'
+  }, 
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/o9OKe3M06QMLOzTl3l6GStYtnE9.jpg'
+  }
+  ],
+  moiveData :[
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/5pAGnkFYSsFJ99ZxDIYnhQbQFXs.jpg'
+  }, 
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/biN2sqExViEh8IYSJrXlNKjpjxx.jpg'
+  }, 
+  {
+    img: 'https://image.tmdb.org/t/p/w640_and_h360_bestv2/rXBB8F6XpHAwci2dihBCcixIHrK.jpg'
+  } 
+  ],
+  defaultTvData : [
+  {
+    id: 1402, 
+    name: '行尸走肉', 
+    img: defaultImg
+  }, 
+  {
+
+    id: 60735, 
+    name: '闪电侠', 
+    img: defaultImg
+  }, 
+  {
+    id: 1622, 
+    name: '邪恶力量', 
+    img: defaultImg
+  }
+  ],
+  defaultMoiveData : [
+  {
+    id: 1402, 
+    name: '金刚狼3：殊死一战', 
+    img: defaultImg
+  }, 
+  {
+
+    id: 60735, 
+    name: '午夜凶铃3(美版)', 
+    img: defaultImg
+  }, 
+  {
+    id: 1622, 
+    name: '五十度黑', 
+    img: defaultImg
+  }
+  ]
+}
+
+export default indexData
