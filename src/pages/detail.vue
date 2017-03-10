cda27d2eb7f3cb78acabe890752c57b67f2223d9
xiaobai <948206388@qq.com>
2017-03-10 18:47:16 +0800

    add detail

---
+++ src/pages/detail.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="detail">
+    <div class="single_column">
+      <ul class="shortcut_bar">
+        <li title="讨论">讨论</li>
+        <li title="视频">视频</li>
+        <li title="图片">图片</li>
+        <li title="共享">共享</li>
+        <li title="报告">报告</li>
+        <li title="变更">变更</li>
+        <li title="编辑">编辑</li>
+      </ul>
+      <div class="poster">
+        <div class="image_content">
+          <img class="backdrop" src="" alt="">
+          <img class="poster" src="" alt="">
+        </div>
+      </div>
+
+      <div class="header poster">
+        <div class="title">
+          <div class="mobile_header">
+            
+          </div>
+          <div class="second_header">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
